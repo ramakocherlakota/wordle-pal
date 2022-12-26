195f943137cda972903632a308f92a70dd950a6a
Rama Kocherlakota <ramakocherlakota@gmail.com>
2022-12-26 17:33:28 -0500

    setup for using lcp

--- wordle-react-app/src/Remainder.js
+++ wordle-react-app/src/Remainder.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import QueryGuessScores from './QueryGuessScores';
 
-export default function Remainder({guessScores, setGuessScores}) {
+export default function Remainder({ guessScores, setGuessScores }) {
     const headerLabels = {"word" : ""};
 
     return <QueryGuessScores guessScores={guessScores} setGuessScores={setGuessScores} operation="remaining_answers" headers={["word"]} headerLabels={ headerLabels } />;
--- wordle-react-app/src/setupProxy.js
+++ wordle-react-app/src/setupProxy.js
@@ -1,10 +1,13 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+// using lcp (https://www.npmjs.com/package/local-cors-proxy) to avoid cors issues
+// lcp --proxyUrl https://wordle-svc.ramakocherlakota.net
+
 module.exports = function(app) {
   app.use(
     '/service',
     createProxyMiddleware({
-      target: 'https://wordle-svc.ramakocherlakota.net',
+      target: 'http://localhost:8010/proxy', 
       changeOrigin: true
     })
   );
