2ca94519c58300a1e4aac34f77d2a0fcc58e826c
Rama Kocherlakota <ramakocherlakota@gmail.com>
2023-01-02 16:04:19 -0500

    CORS issues fixed

--- wordle-react-app/src/setupProxy.js
+++ wordle-react-app/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/service',
     createProxyMiddleware({
-      target: "https://mmsojeihglwc4nrgbkdz5yevge0tdskz.lambda-url.us-east-1.on.aws/",
+      target: "https://kxk4tebf2oubdbadxazw5uuvma0dvfpe.lambda-url.us-east-1.on.aws/",
       changeOrigin: true
     })
   );
