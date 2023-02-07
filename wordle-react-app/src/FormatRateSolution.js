7fcd49c677430d5b78c8aa815c523651dfee0d81
Rama Kocherlakota <ramakocherlakota@gmail.com>
2023-02-06 21:08:56 -0500

    pretty dorky popups for displaying data

--- wordle-react-app/src/FormatRateSolution.js
+++ wordle-react-app/src/FormatRateSolution.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PopupDoc from './PopupDoc';
 
 export default function FormatRateSolution(data) {
   function union_lists(lists) {
@@ -9,6 +10,24 @@ export default function FormatRateSolution(data) {
         return acc;
       }
     }, []);
+  }
+
+  function formatCell(cell) {
+    return (
+      <div>
+        Remaining Answers Before Guess: {cell.remaining_answers_prior}
+        <br/>
+        Remaining Answers After Guess: {cell.remaining_answers_post}
+        <br/>
+        Uncerttainty Before Guess: {cell.uncertainty_prior.toFixed(2)}
+        <br/>
+        Expected Uncertainty After Guess: {cell.exp_uncertainty_post.toFixed(2)}
+        <br/>
+        Uncerttainty After Guess: {cell.uncertainty_post.toFixed(2)}
+        <br/>
+        Luck: {cell.luck.toFixed(2)}
+      </div>
+    );
   }
 
   function table_row(guess, targets, by_target) {
@@ -23,32 +42,50 @@ export default function FormatRateSolution(data) {
       }
     })
     const filling = row_data.map(cell => {
+      const anchors = "anchors!";
       return 'score' in cell
-        ? <td>{cell.score}</td>
-        : <td />;
+        ? <>
+            <div className='col'>
+              <strong>{cell.guess}</strong>
+            </div>
+            <div className='col'></div>
+            <PopupDoc doc={formatCell(cell)} 
+              label={cell.score}
+            />
+            <div className='col'>
+              {anchors}
+            </div>
+          </>
+        : <div />;
     });
 
     return (
-      <tr key={guess} >
+      <div className='row' key={guess} >
         {filling}
-      </tr>
+      </div>
     );
-//    return (
-//      <div key={guess} className='row'>
-//        {
-//        } 
-//     </div>
-//    );
+  }
+
+  function getHeaderRow(targets) {
+    return (
+      <div className='row'>
+        <div className='col'/>
+        {targets.map(target => <div className='col'><strong>{target}</strong></div>)}
+        <div className='col'>Buttons</div>
+      </div>
+    );
   }
 
   if (data.output && typeof data.output === 'object' && 'by_target' in data.output ) {
     const targets = Object.keys(data.output.by_target);
     const guesses = union_lists(Object.values(data.output.by_target).map(records => records.map(record=>record.guess)))
+    const header_row = getHeaderRow(targets);
     const rows = guesses.map(guess => table_row(guess, targets, data.output.by_target));
     return (
-      <table>
+      <>
+        {header_row}
         {rows}
-      </table>
+      </>
     );
   }
 }
