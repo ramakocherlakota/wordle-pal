fcd3fe26294273d7f56216723298ba8ab5c0ddf0
Rama Kocherlakota <ramakocherlakota@gmail.com>
2023-03-01 21:22:35 -0500

    fix a bomb

--- wordle-react-app/src/ScoreSelect.js
+++ wordle-react-app/src/ScoreSelect.js
@@ -22,9 +22,13 @@ export default function ScoreSelect({ value, onChange, placeholder }) {
     }, 100);
   };
 
-  return (
-    <PopupDoc doc=<div>B? W? Dash?<br/><br/>We use the old MasterMind scoring system - a B (black) peg for a letter in the correct position, a W (white) peg for a correct letter in the wrong position.  <br/><br/>So 'B-W--' means the first letter is correct and the third letter is in the answer but in a different position.</div>>
-      <AsyncSelect placeholder={placeholder} defaultOptions={scoreOptions} loadOptions={loadOptions} onChange={onChange} value={filterOptions(value)} />
-    </PopupDoc>
-  );
+  if (value) {
+    return (
+      <PopupDoc doc=<div>B? W? Dash?<br/><br/>We use the old MasterMind scoring system - a B (black) peg for a letter in the correct position, a W (white) peg for a correct letter in the wrong position.  <br/><br/>So 'B-W--' means the first letter is correct and the third letter is in the answer but in a different position.</div>>
+        <AsyncSelect placeholder={placeholder} defaultOptions={scoreOptions} loadOptions={loadOptions} onChange={onChange} value={filterOptions(value)} />
+      </PopupDoc>
+    )
+  } else {
+    return null;
+  }  
 }
